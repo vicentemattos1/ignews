a6457eff13d1ab63dd3d10ecb7adb01395b2e610
Vicente Mattos <vicentemattos1@hotmail.com>
2021-07-03 13:47:06 -0300

    Pagamento no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +8,37 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    // Caso o usuário não esteja logado, ele vai redirecionar para fazer o login
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      // Faz a requisição para a Api route para criar um customer no stripe
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      // O getStripeJs é uma funcionalidade fornecida para o front-end do Stripe, que serve para redirecionar para o meio de pagamento
+      const stripe = await getStripeJs();
+
+      // Caso dê tudo certo, redireciona o usuário para o Checkout para efetuar o pagamento
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={() => handleSubscribe()}
+    >
       Subscribe Now
     </button>
   );
